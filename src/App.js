3f929352d1598c7f2970c3a9acbbf84dfc3b5fe8
Nicholas Darash <nickdarash@gmail.com>
2019-04-24 00:57:47 -0400

    Added countdown timer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import Main from './components/main/main';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount(){
+    document.title = 'Nick Darash | Coming Soon'
+  }
+
   render() {
     return (
       <div className="root">
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,8 +5,42 @@ class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
-            'Test': 'test'
+            'countdown': {
+                'Days': null,
+                'TensOfDays': null,
+                'Hours': null,
+                'TensOfHours': null,
+                'Minutes': null,
+                'TensOfMinutes': null,
+                'Seconds': null,
+                'TensOfSeconds': null
+            }
         }
+    }
+
+    componentDidMount(){
+        setInterval(() => {
+            const estimatedDate = new Date("May 25, 2019 12:00:00").getTime();
+            const now = new Date().getTime();
+            const time = estimatedDate - now;
+            const days = Math.floor(time/(1000*60*60*24));
+            const hours = Math.floor((time % (1000*60*60*24)) / (1000*60*60));
+            const minutes = Math.floor((time % (1000*60*60)) / (1000*60));
+            const seconds = Math.floor((time % (1000*60)) / 1000);
+            this.setState({
+                countdown:{
+                    'Days': days%10,
+                    'TensOfDays': Math.floor(days/10),
+                    'Hours': hours%10,
+                    'TensOfHours': Math.floor(hours/10),
+                    'Minutes': minutes%10,
+                    'TensOfMinutes': Math.floor(minutes/10),
+                    'Seconds': seconds%10,
+                    'TensOfSeconds': Math.floor(seconds/10)
+                }
+            })
+            //console.log(this.state);
+        }, 1000);
     }
 
     render() {
@@ -19,9 +53,27 @@ class Main extends Component {
                         learn and associate with my accomplishments.
                     </p>
                     <form action="#">
-                        <input type="email" value="" placeholder="Email"></input>
+                        <input type="email" placeholder="Email"></input>
                         <input type="submit"></input>
                     </form>
+                    <div className="countdown">
+                        <div className="timer-module">
+                            <div className="ten">{this.state.countdown.TensOfDays}</div>
+                            <div className="one">{this.state.countdown.Days}</div>
+                        </div>
+                        <div className="timer-module">
+                            <div className="ten">{this.state.countdown.TensOfHours}</div>
+                            <div className="one">{this.state.countdown.Hours}</div>
+                        </div>
+                        <div className="timer-module">
+                            <div className="ten">{this.state.countdown.TensOfMinutes}</div>
+                            <div className="one">{this.state.countdown.Minutes}</div>
+                        </div>
+                        <div className="timer-module">
+                            <div className="ten">{this.state.countdown.TensOfSeconds}</div>
+                            <div className="one">{this.state.countdown.Seconds}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
